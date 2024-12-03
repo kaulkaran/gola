8738abe1ae5e942ecb50883df6f3c367e7326db0
Arun <ak1577282@gmail.com>
2024-12-03 17:38:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.use(bodyParser.json());
+
+const SECRET_KEY = process.env.RAZORPAY_SECRET;
+
+app.post('/verify-payment', (req, res) => {
+    const secret = SECRET_KEY; // Replace with your Razorpay secret
+    const { payment_id, order_id, signature } = req.body;
+
+    // Construct the string to be verified
+    const hmac = crypto.createHmac('sha256', secret);
+    const generated_signature = hmac.update(`${order_id}|${payment_id}`).digest('hex');
+
+    if (signature === generated_signature) {
+        console.log('Payment verification successful');
+        res.status(200).send({ success: true });
+    } else {
+        console.log('Payment verification failed');
+        res.status(400).send({ success: false });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
